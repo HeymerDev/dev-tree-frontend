3904db1700ef932dbbc4e9025b518ba701cf8cb6
HeymerDev <heymermeza11@gmail.com>
2026-01-13 21:33:55 -0500

    modified view devtree

--- src/pages/app/LinkTree.tsx
+++ src/pages/app/LinkTree.tsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+import { social } from "../../data/social";
+import type { DevTreeLink } from "../../types";
+import { DevTreeLinks } from "../../components/links/DevTreeLinks";
+import { isValidUrl } from "../../utils";
+import { toast } from "sonner";
+
 const LinkTree = () => {
-  return <div>LinkTree</div>;
+  const [links, setLinks] = useState<DevTreeLink[]>(social);
+
+  const handleUrlChange = (name: string, url: string) => {
+    setLinks((prevLinks) =>
+      prevLinks.map((link) => (link.name === name ? { ...link, url } : link))
+    );
+  };
+
+  const handleEnableLink = (socialName: string) => {
+    const linkToToggle = links.find((link) => link.name === socialName);
+    if (!linkToToggle) return;
+
+    if (!isValidUrl(linkToToggle.url)) {
+      toast.error("Please enter a valid URL before enabling the link.");
+      return;
+    }
+
+    setLinks((prevLinks) =>
+      prevLinks.map((link) =>
+        link.name === socialName ? { ...link, enabled: !link.enabled } : link
+      )
+    );
+  };
+
+  return (
+    <div className="space-y-5">
+      {links.map((link) => (
+        <DevTreeLinks
+          key={link.name}
+          link={link}
+          handleUrlChange={handleUrlChange}
+          handleEnableLink={handleEnableLink}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default LinkTree;
--- src/types/index.ts
+++ src/types/index.ts
@@ -21,3 +21,12 @@ export type EditProfileFormData = Pick<User, "handle" | "description">;
 export type EditProfileResponse = {
   message: string;
 };
+
+export type SocialNetwork = {
+  id: string;
+  name: string;
+  url: string;
+  enabled: boolean;
+};
+
+export type DevTreeLink = Omit<SocialNetwork, "id">;
